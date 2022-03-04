0c71353fdfb304a75b325af33d080051db86427b
shivenj <shiven.juneja@gmail.com>
2022-03-04 15:08:23 +0530

    update database file

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'ec2-44-194-113-156.compute-1.amazonaws.com'),
+      host: env('DATABASE_HOST', 'ec2-52-204-196-4.compute-1.amazonaws.com'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'dbg3ktnnugeg58'),
-      user: env('DATABASE_USERNAME', 'hqsjtvrnctlczj'),
-      password: env('DATABASE_PASSWORD', 'b8935cf9c9bdd4cc256ea7f43dcec83cfc76e3a54bb9efe19122a18915acc587'),
+      database: env('DATABASE_NAME', 'dejqrcpfu9k1hq'),
+      user: env('DATABASE_USERNAME', 'thwcywdeerztpa'),
+      password: env('DATABASE_PASSWORD', '994d67c6f9850f93b6ae43dc7a4ba446e6c067abf075fa12c5f16f68f5606d7b'),
       ssl: env.bool('DATABASE_SSL', false),
       ssl: {
         rejectUnauthorized: false
