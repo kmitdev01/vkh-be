aaf315d16a1cbb96313986cfb59ab426efebfab5
shivenj <shiven.juneja@gmail.com>
2022-03-09 14:58:50 +0530

    sorting implimented on all filters

--- src/api/report/controllers/filter.js
+++ src/api/report/controllers/filter.js
@@ -6,14 +6,14 @@ module.exports = createCoreController('api::report.report', ({ strapi }) => ({
     // Method 2: Wrapping a core service (leaves core logic in place)
     async getFilters(ctx) {
         const results = {};
-        const countries = await strapi.db.query('api::country.country').findMany();
-        const years = await strapi.db.query('api::year.year').findMany();
-        const generationSources = await strapi.db.query('api::generation-source.generation-source').findMany();
-        const publishingOrganization = await strapi.db.query('api::publishing-organization.publishing-organization').findMany();
-        const region = await strapi.db.query('api::region.region').findMany();
-        const segment = await strapi.db.query('api::segment.segment').findMany();
-        const documentType = await strapi.db.query('api::document-type.document-type').findMany();
-        const coAuthor = await strapi.db.query('api::co-author.co-author').findMany();
+        const countries = await strapi.db.query('api::country.country').findMany({ orderBy: { CountryName: 'ASC' } });
+        const years = await strapi.db.query('api::year.year').findMany({ orderBy: { Year: 'ASC' } });
+        const generationSources = await strapi.db.query('api::generation-source.generation-source').findMany({ orderBy: { GenerationSource: 'ASC' } });
+        const publishingOrganization = await strapi.db.query('api::publishing-organization.publishing-organization').findMany({ orderBy: { PublishingOrganization: 'ASC' } });
+        const region = await strapi.db.query('api::region.region').findMany({ orderBy: { RegionName: 'ASC' } });
+        const segment = await strapi.db.query('api::segment.segment').findMany({ orderBy: { SegmentName: 'ASC' } });
+        const documentType = await strapi.db.query('api::document-type.document-type').findMany({ orderBy: { DocumentType: 'ASC' } });
+        const coAuthor = await strapi.db.query('api::co-author.co-author').findMany({ orderBy: { AuthorName: 'ASC' } });
 
         results.countries = countries;
         results.year = years;
