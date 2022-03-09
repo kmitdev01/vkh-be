c2c6fca85a6c428c5e60aae99a467e3e3ec5f9fb
shivenj <shiven.juneja@gmail.com>
2022-03-09 15:35:47 +0530

    added question key in question list API

---
+++ src/api/question-type-key/controllers/question-type-key.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  question-type-key controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::question-type-key.question-type-key');
