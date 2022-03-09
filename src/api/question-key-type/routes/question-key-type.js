08ff69f255ce880915246881ed75fff949278d1c
shivenj <shiven.juneja@gmail.com>
2022-03-09 16:08:22 +0530

    added new key for question list API

---
+++ src/api/question-key-type/routes/question-key-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * question-key-type router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::question-key-type.question-key-type');
