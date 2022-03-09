593f4aa6bcc775abbc3f6e8c34e2d424d66ec783
shivenj <shiven.juneja@gmail.com>
2022-03-09 15:50:00 +0530

    remove some files

--- src/api/question-type-key/controllers/question-type-key.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  question-type-key controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::question-type-key.question-type-key');
--- src/api/question-type-key/routes/question-type-key.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * question-type-key router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::question-type-key.question-type-key');
